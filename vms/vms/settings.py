d560024afb36db574a75c11620b8746249bf7061
anjali-dhanuka <anjalidhanuka99@gmail.com>
2018-02-01 13:28:44 +0530

    manage.py modified
    travis files fixed
    
    manage.py restored
    
    auth_user relation error fixed
    
    user replaced
    
    makemigrations sorted

--- vms/vms/settings.py
+++ vms/vms/settings.py
@@ -8,7 +8,7 @@ from django.utils.translation import ugettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
