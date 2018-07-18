85d8894958e8613954f7359b1d2a0d55007f6491
anjali-dhanuka <anjalidhanuka99@gmail.com>
2018-07-19 02:41:18 +0530

    formatting done

--- vms/pom/locators/administratorReportPageLocators.py
+++ vms/pom/locators/administratorReportPageLocators.py
@@ -3,5 +3,5 @@ class AdministratorReportPageLocators(object):
     HOURS = '//table//tbody//tr[1]//td[4]'
     VIEW_REPORT = '//table//tbody//tr[1]//td[5]//a'
     REJECT_REPORT = '//table//tbody//tr[1]//td[6]'
-    APPROVE_REPORT = '//div[2]//div[2]'    
+    APPROVE_REPORT = '//div[2]//div[2]'
     REPORT_SHIFT_SUMMARY_PATH = '//div[2]/div[3]'
